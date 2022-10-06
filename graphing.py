6ef98a0b8970260805b1ab695a0c043903621caf
Shashank <shashviru@gmail.com>
2022-10-06 20:46:04 +0530

    graphing

---
+++ graphing.py
@@ -0,0 +1,22 @@
+import sys
+import math
+import matplotlib.pyplot as plt
+  
+
+x = [1,2,3]
+
+y = [2,4,1]
+  
+
+plt.plot(x, y)
+  
+
+plt.xlabel('x - axis')
+
+plt.ylabel('y - axis')
+  
+
+plt.title('My first graph!')
+  
+
+plt.show()
